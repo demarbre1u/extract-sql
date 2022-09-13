13189ada54ae09dcb84c8ddf3b5dd896874e2fea
Allan Demarbre <ademarbre@groupe-cac.com>
2022-09-13 17:59:05 +0200

    Added colors to the help menu

--- extract-sql.sh
+++ extract-sql.sh
@@ -6,23 +6,23 @@ OUTPUT_FILE="extract.sql"
 
 function printHelp() {
 	echo ""
-	echo "This bash script searches for a given SQL query in a given file, and extracts it."
+	echo -e "\033[0;37mThis bash script searches for a given SQL query in a given file, and extracts it."
 	echo ""
-	echo "Usage:"
+	echo -e "\033[0;36mUsage:\033[0;37m"
 	echo -e "\t./extract_sql.sh [-h|--help] [-q|--query=query] [-i|--input=input_file] [-o|--o=output_file]"
 	echo ""
-	echo "Options:"
+	echo -e "\033[0;36mOptions:\033[0;37m"
 	echo -e "\t-h, --help"
-	echo -e "\t\tDisplay this menu"
+	echo -e "\t\t\033[0;0mDisplay this menu\033[0;37m"
 	echo ""
 	echo -e "\t-q, --query"
-	echo -e "\t\tRequired. The keyword to look for in the specified file"
+	echo -e "\t\tRequired. \033[0;0mThe keyword to look for in the specified file\033[0;37m"
 	echo ""
 	echo -e "\t-i, --input"
-	echo -e "\t\tRequired. The file to search into"
+	echo -e "\t\tRequired. \033[0;0mThe file to search into\033[0;37m"
 	echo ""
 	echo -e "\t-o, --output"
-	echo -e "\t\tThe file to write the result into"
+	echo -e "\t\t\033[0;0mThe file to write the result into\033[0;37m"
 }
 
 # Loop that parses the args / options of the command
@@ -45,12 +45,12 @@ for i in "$@"; do
       		shift
       		;;
 		-*|--*)
-      		echo "ERROR: Unknown option $i"
+      		echo -e "\033[0;31mERROR\033[0;37m: Unknown option $i"
 	  		printHelp
       		exit 1
       		;;
     	*)
-			echo "ERROR: Unexpected value $i"
+			echo -e "\033[0;31mERROR\033[0;37m: Unexpected value $i"
 	  		printHelp
       		exit 2
       		;;
@@ -65,20 +65,20 @@ fi
 
 # query and input are required options
 if [ -z "$QUERY" ]; then 
-	echo "ERROR: The [-q|--query] option is required"
+	echo -e "\033[0;31mERROR\033[0;37m: The [-q|--query] option is required"
 	printHelp
 	exit 3
 fi
 
 if [ -z "$INPUT_FILE" ]; then 
-	echo "ERROR: The [-i|--input] option is required"
+	echo -e "\033[0;31mERROR\033[0;37m: The [-i|--input] option is required"
 	printHelp
 	exit 3
 fi
 
 # Checks if the input option is a valid file
 if [ ! -f "$INPUT_FILE" ]; then
-	echo "ERROR : input is not a file"
+	echo -e "\033[0;31mERROR\033[0;37m : input is not a file"
 	printHelp
 	exit 3
 fi
